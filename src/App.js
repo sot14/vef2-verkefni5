e1585a80d01f43d3eb38a67a56a2ca9fae2d7c0b
sothorsteins <sothorsteins@gmail.com>
2021-03-23 14:13:13 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// TODO sækja og setja upp react router
+
+import { Layout } from './components/layout/Layout';
+
+import { Index } from './pages/Index';
+import { NewsPage } from './pages/News';
+import { NotFound } from './pages/NotFound';
+
+export default function App() {
+  return (
+    <Layout>
+      ...
+    </Layout>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import './index.css';
+
+// TODO setja upp router
+
+ReactDOM.render(
+  <React.StrictMode>
+
+      <App />
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
